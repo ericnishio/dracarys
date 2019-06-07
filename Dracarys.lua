aede7ce50c45d98f8a29ef9fd9d8949ac701accd
Eric Nishio <ericnishio@protonmail.com>
2019-06-07 17:37:05 +0300

    Clean up code

--- Dracarys.lua
+++ Dracarys.lua
@@ -3,10 +3,29 @@ local frame = CreateFrame('FRAME')
 frame:RegisterEvent('ADDON_LOADED')
 frame:RegisterEvent('PLAYER_ENTERING_WORLD')
 
+local DEFAULT = 0
+local RARE = 1
+local RARE_ELITE = 2
+local ELITE = 3
+
+local INDEX_TO_TEXTURE_MAP = {
+  [DEFAULT] = 'Interface\\TargetingFrame\\UI-TargetingFrame.blp',
+  [RARE] = 'Interface\\TargetingFrame\\UI-TargetingFrame-Rare.blp',
+  [RARE_ELITE] = 'Interface\\TargetingFrame\\UI-TargetingFrame-Rare-Elite.blp',
+  [ELITE] = 'Interface\\TargetingFrame\\UI-TargetingFrame-Elite.blp',
+}
+
+local INDEX_TO_LABEL_MAP = {
+  [DEFAULT] = 'Default',
+  [RARE] = 'Rare',
+  [RARE_ELITE] = 'Rare Elite',
+  [ELITE] = 'Elite',
+}
+
 function frame:OnEvent(event, arg1)
   if event == 'ADDON_LOADED' and arg1 == 'DracarysTextureIndex' then
     if DracarysTextureIndex == nil then
-      DracarysTextureIndex = 1
+      DracarysTextureIndex = RARE -- first-time value
     end
   end
 
@@ -19,51 +38,35 @@ frame:SetScript('OnEvent', frame.OnEvent)
 
 SLASH_DRACARYS1 = '/dracarys'
 
-SlashCmdList['DRACARYS'] = function(msg)
-  local i = tonumber(msg)
+SlashCmdList['DRACARYS'] = function(value)
+  local index = tonumber(value)
 
-  if i == nil or i < 0 or i > 3 then
-    Dracarys_Help()
+  if (INDEX_TO_TEXTURE_MAP[index] == nil) then
+    Dracarys_PrintHelp()
     return
   end
 
-  DracarysTextureIndex = i
+  Dracarys_PrintHelp()
 
-  Dracarys_PortraitChangedMessage(i)
+  DracarysTextureIndex = index
 
-  Dracarys_ChangePortrait(i)
+  Dracarys_PrintChanged(index)
+
+  Dracarys_ChangePortrait(index)
 end
 
-function Dracarys_ChangePortrait(option)
-  local i = tonumber(option)
-
-  local textures = {
-    [0] = 'Interface\\TargetingFrame\\UI-TargetingFrame.blp',
-    [1] = 'Interface\\TargetingFrame\\UI-TargetingFrame-Rare.blp',
-    [2] = 'Interface\\TargetingFrame\\UI-TargetingFrame-Rare-Elite.blp',
-    [3] = 'Interface\\TargetingFrame\\UI-TargetingFrame-Elite.blp',
-  }
-
-  local texture = textures[i]
+function Dracarys_ChangePortrait(index)
+  local texture = INDEX_TO_TEXTURE_MAP[index]
 
   PlayerFrameTexture:SetTexture(texture)
 end
 
-function Dracarys_PortraitChangedMessage(option)
-  local messages = {
-    [0] = 'None',
-    [1] = 'Rare',
-    [2] = 'Rare Elite',
-    [3] = 'Elite',
-  }
-
-  print('Player portrait set to ' .. messages[option])
+function Dracarys_PrintChanged(index)
+  print('Player portrait changed to ' .. INDEX_TO_LABEL_MAP[index])
 end
 
-function Dracarys_Help()
-  print('Commands:\n');
-  print('/dracarys 1 (Rare)');
-  print('/dracarys 2 (Rare Elite)');
-  print('/dracarys 3 (Elite)');
-  print('/dracarys 0 (None)');
+function Dracarys_PrintHelp()
+  for key, value in pairs(INDEX_TO_LABEL_MAP) do
+    print('/dracarys ' .. key .. ' (' .. value ..')')
+  end
 end
